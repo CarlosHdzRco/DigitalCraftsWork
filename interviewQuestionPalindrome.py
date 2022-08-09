c51d33c4d4f13c62885fcb09a2ce549a6035cf1b
Carlos Hernandez <carloshdzrco@gmail.com>
2022-08-09 09:55:09 -0500

    adjusted palindrome problem

--- interviewQuestionPalindrome.py
+++ interviewQuestionPalindrome.py
@@ -30,3 +30,32 @@ elif(isPalindrome == False):
 #     print('%s is not a palindrome' % userNum)
 
 # # Haven't complete for non-numbers input
+
+
+
+# word = input("Enter a word. ")
+# half = len(word)/2
+# half = int(half)
+# print(word[0:half+1])
+# print(word[len(word):half-1:-1])
+
+# if word[0:half+1] == word[len(word):half-1:-1]:
+#     print("It is a palindrome. ")
+# else:
+#     print("Not a palindrome. ")
+
+
+# numbers = (input('Please enter a number that has at least three characters: '))
+
+# try:
+#     checkInt = int(numbers)
+#     if len(numbers) > 2:
+#         palindrome = numbers[::-1]
+#         if numbers == palindrome:
+#             print('The number %s is a palindrome!' % numbers)
+#         else:
+#             print('The number %s is not a palindrome!' % numbers)
+#     else:
+#         print('Invalid entry.')
+# except:
+#     print('Invalid entry.')
